6e8d13cc6067e36c9d3edc5e14001d5066e07e69
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2019-01-16 12:59:16 +0100

    Fix json serialization for nested

--- evalg/database/types.py
+++ evalg/database/types.py
@@ -9,9 +9,13 @@ import flask.json
 import sqlalchemy.util
 import sqlalchemy_utils
 import sqlalchemy_utils.types.json
+
+from graphene.types.generic import GenericScalar
 from graphene_sqlalchemy.converter import convert_sqlalchemy_type
 from graphene_sqlalchemy.converter import convert_column_to_string
 from graphene_sqlalchemy.converter import convert_json_to_string
+from graphene_sqlalchemy.converter import get_column_doc
+from graphene_sqlalchemy.converter import is_column_nullable
 from sqlalchemy_json import MutableJson
 from sqlalchemy_json import NestedMutableJson
 
@@ -41,11 +45,17 @@ class JSONType(sqlalchemy_utils.types.json.JSONType):
         return sqlalchemy.util.generic_repr(self)
 
 
-# Graphene compatibility:
+@convert_sqlalchemy_type.register(JSONType)
+@convert_sqlalchemy_type.register(MutableJson)
+@convert_sqlalchemy_type.register(NestedMutableJson)
+def convert_json_to_generic_scalar(type, column, registry=None):
+    return GenericScalar(description=get_column_doc(column),
+        required=not(is_column_nullable(column)))
 
+
+# Graphene compatibility:
 convert_sqlalchemy_type.register(URLType)(convert_column_to_string)
 convert_sqlalchemy_type.register(UUIDType)(convert_column_to_string)
-convert_sqlalchemy_type.register(JSONType)(convert_json_to_string)
 
 
 __all__ = [
