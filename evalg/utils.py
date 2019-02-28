63e77f817389eeb49751a0f9a2100eb931af938e
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-02-28 16:44:55 +0100

    Add enum subtype
    Adds a function that creates an enum type with a `get_description()`
    method from a dict of {value: description} pairs.
    
    Each enum in the enum type will have its name set to its value.

--- evalg/utils.py
+++ evalg/utils.py
@@ -1,7 +1,7 @@
 import datetime
+import enum
 import re
 
-from flask import _request_ctx_stack
 from collections.abc import Iterable
 
 under_pat = re.compile(r'_([a-z])')
@@ -28,8 +28,59 @@ def convert_json_internal(data, convert):
 
 
 def iterable_but_not_str(obj):
-    return isinstance(obj, Iterable) and not isinstance(obj, (str, bytes, bytearray))
+    return (
+        isinstance(obj, Iterable) and
+        not isinstance(obj, (str, bytes, bytearray)))
 
 
 def utcnow():
     return datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+
+
+def make_descriptive_enum(name, values):
+    """
+    Make an enum of string constants with descriptions.
+
+    >>> Values = make_descriptive_enum(
+    ...     'Values',
+    ...     {None: 'some values', 'foo': 'a foo value', 'bar': 'a bar value'})
+    <enum 'Values'>
+    >>> Values.foo
+    <Values.foo: 'foo'>
+    >>> Values('foo')
+    <Values.foo: 'foo'>
+    >>> Values.get_description()
+    'some values'
+    >>> Values.get_description('bar')
+    'a bar value'
+    >>> Values.foo.get_description()
+    'a foo value'
+
+    :type name: str
+    :param name: Name of the enum
+
+    :type values: dict
+    :param values:
+        A mapping of enum values and their description.
+        The special key ``None`` will provide a description of the enum class.
+
+    :rtype: enum.EnumMeta
+    :return: An enum class with values and descriptions
+    """
+
+    def get_description(value=None):
+        print('get_description(%r)' % (value, ))
+        if isinstance(value, enum.Enum):
+            value = value.value
+        return values.get(value, '')
+
+    _description_mixin = type(
+        'DescriptiveEnum',
+        (object, ),
+        {'get_description': get_description})
+
+    return enum.unique(
+        enum.Enum(
+            name,
+            {v: v for v in values if v is not None},
+            type=_description_mixin))
