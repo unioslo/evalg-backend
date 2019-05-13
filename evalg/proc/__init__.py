14ea9e31897e9343c5dd39e54234ea276356d73a
Jonas Braathen <jonas.braathen@usit.uio.no>
2019-05-13 17:24:54 +0200

    Move evalg.group to evalg.proc.group

--- evalg/proc/__init__.py
+++ evalg/proc/__init__.py
@@ -3,11 +3,15 @@ evalg processes
 
 These modules implements the evalg business logic layer.
 """
+from . import group
+from . import count
 from . import pollbook
 from . import vote
 
 
 __all__ = [
+    'count',
+    'group',
     'pollbook',
     'vote',
 ]
