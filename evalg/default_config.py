57f93cdbbf0e7e6fe2ef6a3910cbc3a3d73c25b6
Marius Jean Bischof Hagalid <marius.hagalid@usit.uio.no>
2019-09-04 12:06:38 +0200

    Move permissions config to default_config
    Issue: EVALG-590

--- evalg/default_config.py
+++ evalg/default_config.py
@@ -44,7 +44,6 @@ TRUSTED_PROXIES = (
     '::1',
 )
 
-
 #
 # Feide auth configuration
 #
@@ -58,7 +57,98 @@ FEIDE_BASIC_USERS = []
 #
 # Permissions
 #
-PERMISSIONS_PATH = 'evalg/authorization/permissions_config.json'
+PERMISSIONS = {
+    'ObjectTypes': {
+        'ElectionGroup': {
+            'Fields': {
+                'name': 'allow',
+                'description': 'allow',
+                'type': 'allow',
+                'meta': 'allow',
+                'ou_id': 'allow',
+                'elections': 'allow',
+                'active': 'allow',
+                'public_key': 'can_manage_election_group',
+                'announced_at': 'allow',
+                'published_at': 'allow',
+                'cancelled_at': 'allow',
+                'deleted_at': 'can_manage_election_group',
+                'announced': 'allow',
+                'published': 'allow',
+                'cancelled': 'allow',
+                'deleted': 'allow',
+                'status': 'allow',
+                'election_group_counts': 'can_manage_election_group'
+            }
+        },
+        'Election': {
+            'Fields': {
+                'name': 'allow',
+                'description': 'allow',
+                'meta': 'allow',
+                'active': 'allow',
+                'announced_at': 'allow',
+                'published_at': 'allow',
+                'cancelled_at': 'allow',
+                'announced': 'allow',
+                'published': 'allow',
+                'cancelled': 'allow',
+                'deleted': 'allow',
+                'vote_count': 'can_manage_election',
+                'status': 'allow',
+                'pollbooks': 'allow',
+                'election_results': 'can_manage_election'
+            }
+        },
+        'ElectionGroupCount': {
+            'Fields': {
+                'group_id': 'can_access_election_group_count',
+                'election_group': 'can_access_election_group_count',
+                'election_results': 'can_access_election_group_count',
+                'initiated_at': 'can_access_election_group_count',
+                'finished_at': 'can_access_election_group_count',
+                'audit': 'can_access_election_group_count',
+                'status': 'can_access_election_group_count',
+            }
+        },
+        'ElectionResult': {
+            'Fields': {
+                'election_id': 'can_access_election_result',
+                'election': 'can_access_election_result',
+                'election_group_count_id': 'can_access_election_result',
+                'election_group_count': 'can_access_election_result',
+                'election_protocol': 'can_access_election_result',
+                'ballots': 'can_access_election_result',
+                'result': 'can_access_election_result',
+                'pollbook_stats': 'can_access_election_result',
+                'election_protocol_text': 'can_access_election_result',
+            }
+        },
+        'ElectionList': {
+            'Fields': {
+                'name': 'allow',
+                'description': 'allow',
+                'information_url': 'allow',
+                'election_id': 'allow',
+                'election': 'allow',
+                'candidates': 'allow',
+            }
+        },
+        'Candidate': {
+            'Fields': {
+                'list_id': 'allow',
+                'list': 'allow',
+                'name': 'allow',
+                'meta': 'allow',
+                'information_url': 'allow',
+                'priority': 'allow',
+                'pre_cumulated': 'allow',
+                'user_cumulated': 'allow',
+            }
+        }
+    },
+
+}
 
 #
 # Ballot encryption/serialization
