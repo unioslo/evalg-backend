f1379ed573ff7d6c80a9a5d2692957f1ee8f1dcb
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2019-09-10 10:54:45 +0200

    Get group by name

--- evalg/proc/group.py
+++ evalg/proc/group.py
@@ -15,6 +15,11 @@ def search_groups(session, filter_string):
     return session.query(Group).filter(func.lower(Group.name).like(f))
 
 
+def get_group_by_name(session, group_name):
+    """Get at group by it's name."""
+    return session.query(Group).filter(Group.name == group_name).first()
+
+
 def is_member_of_group(session, group, person):
     """Check if person is a member of a group."""
     return session.query(exists().where(and_(
