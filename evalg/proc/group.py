219e39471bc6dc8025c8137035eaeca8eccca049
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2019-09-11 10:30:28 +0200

    Group handling methods
    Issue: EVALG-132

--- evalg/proc/group.py
+++ evalg/proc/group.py
@@ -5,6 +5,8 @@ from sqlalchemy import func
 from sqlalchemy import exists
 from sqlalchemy import and_
 
+import evalg
+
 from evalg.models.group import Group, GroupMembership
 
 
@@ -16,8 +18,13 @@ def search_groups(session, filter_string):
 
 
 def get_group_by_name(session, group_name):
-    """Get at group by it's name."""
+    """Get a group by it's name."""
     return session.query(Group).filter(Group.name == group_name).first()
+
+
+def get_group_by_id(session, group_id):
+    """Get a group by it's id."""
+    return session.query(Group).filter(Group.id == group_id).first()
 
 
 def is_member_of_group(session, group, person):
@@ -26,3 +33,33 @@ def is_member_of_group(session, group, person):
         GroupMembership.group_id == group.id,
         GroupMembership.person_id == person.id))
     ).scalar()
+
+
+def add_person_to_group(session, group, person):
+    """Add a person to a group."""
+    membership = evalg.database.query.get_or_create(
+        session,
+        GroupMembership,
+        group_id=group.id,
+        person_id=person.id,
+    )
+    session.add(membership)
+    session.flush()
+    return membership
+
+
+def remove_person_from_group(session, group, person):
+    """Remove a person from a group."""
+    membership = session.query(GroupMembership).filter(and_(
+        GroupMembership.group_id == group.id,
+        GroupMembership.person_id == person.id)).first()
+    if membership:
+        session.delete(membership)
+        session.flush()
+
+
+def get_user_groups(session, person):
+    """Get the groups a user is a member of."""
+    memberships = session.query(GroupMembership).filter(
+        GroupMembership.person_id == person.id).all()
+    return [get_group_by_id(session, x.group_id) for x in memberships]
--- tests/test_proc/test_proc_group.py
+++ tests/test_proc/test_proc_group.py
@@ -1,7 +1,14 @@
 from evalg.database.query import get_or_create
 from evalg.models.group import Group
-from evalg.proc.group import is_member_of_group
-from evalg.proc.group import search_groups
+from evalg.models.group import GroupMembership
+
+from evalg.proc.group import (add_person_to_group,
+                              get_group_by_id,
+                              get_group_by_name,
+                              get_user_groups,
+                              is_member_of_group,
+                              remove_person_from_group,
+                              search_groups)
 
 
 def test_search_group(db_session):
@@ -12,14 +19,87 @@ def test_search_group(db_session):
     assert results.count() == 1
 
 
+def test_get_group_by_name(db_session, make_group):
+    """Test get_group_by_name."""
+    group_name = 'test_get_group_by_name'
+    group_error = 'test_get_group_by_name_error'
+    group_foo = make_group(group_name)
+    assert get_group_by_name(db_session, group_name).id == group_foo.id
+    assert get_group_by_name(db_session, group_error) is None
+
+
+def test_get_group_by_id(db_session, make_group):
+    """Test get_group_by_id."""
+    group = make_group('test_get_group_by_id')
+    res = get_group_by_id(db_session, group.id)
+    assert res
+    assert res.id == group.id
+    assert res.name == group.name
+
+
 def test_is_member_of_group(
         db_session,
         make_group,
         make_group_membership,
         persons):
-    """Test is_member_of_group"""
+    """Test is_member_of_group."""
     group = make_group('test_member_of̈́')
     persons = list(persons.values())
-    make_group_membership = make_group_membership(group, persons[0])
+    group_membership = make_group_membership(group, persons[0])
+    assert group_membership
     assert is_member_of_group(db_session, group, persons[0])
     assert not is_member_of_group(db_session, group, persons[1])
+
+
+def test_add_person_to_group(db_session, make_group, make_person):
+    """Test add_person_to_group"""
+    group = make_group('test_add_person_to_group')
+    person = make_person('test_add_person_to_group', 'add@group.no')
+    membership = add_person_to_group(db_session, group, person)
+    assert membership
+    membership_db = GroupMembership.query.get(membership.id)
+    assert membership_db
+    assert membership_db.person_id == membership.person_id
+    assert membership_db.group_id == membership.group_id
+
+
+def test_remove_person_from_group(
+        db_session,
+        make_group,
+        make_group_membership,
+        make_person):
+    """Test remove_person_from_group"""
+    person = make_person('remove_person_from_group', 'remove@group.no')
+    group = make_group('remove_person_from_group_test')
+    membership = make_group_membership(group, person)
+    assert membership
+    remove_person_from_group(db_session, group, person)
+    membership_db = GroupMembership.query.get(membership.id)
+    assert not membership_db
+
+
+def test_get_user_groups(
+        db_session,
+        make_group,
+        make_group_membership,
+        make_person):
+    """Test get_user_groups."""
+    person_a = make_person('test_get_user_groups_a',
+                           'test_get_user_groups@foo.no')
+    person_b = make_person('test_get_user_groups_b',
+                           'test_get_user_groups@bar.no')
+    groups = [make_group('test_get_user_groups_a'),
+              make_group('test_get_user_groups_b')]
+    memberships = [make_group_membership(x, person_a) for x in groups]
+
+    res_a = get_user_groups(db_session, person_a)
+    assert res_a
+    assert len(res_a) == len(groups)
+
+    res_a_ids = [x.id for x in res_a]
+    membership_group_ids = [x.group_id for x in memberships]
+    assert all(x in res_a_ids for x in membership_group_ids)
+
+    res_b = get_user_groups(db_session, person_b)
+    assert not res_b
+    assert len(res_b) == 0
