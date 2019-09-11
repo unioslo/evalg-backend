571c8e6454167cd70398cadd413c0eb373b382a7
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2019-09-11 15:05:49 +0200

    Create the group_principal and roles for the global access roles.
    Issue: EVALG-132

--- evalg/cli/auth_cmds.py
+++ evalg/cli/auth_cmds.py
@@ -10,7 +10,16 @@ from flask import current_app
 import evalg
 
 
-def add_group(group_name):
+# TODO: Use the corresponding get_or_create methods in proc.authz
+#   Importing these methods brakes pytest, as we a missing context in proc.
+#   This can be fixed by not inherit from `db.Model` i models.base.
+
+def get_or_create_group(group_name):
+    """Ensure existence of a group."""
+    group = evalg.models.group.Group.query.filter_by(
+        name=group_name).first()
+    if group:
+        return group
     current_app.logger.info('Adding missing entitlement group, name: %s',
                             group_name)
     group = evalg.models.group.Group()
@@ -19,9 +28,43 @@ def add_group(group_name):
     evalg.db.session.flush()
 
 
-def get_group(group_name):
-    return evalg.models.group.Group.query.filter_by(
-        name=group_name).first()
+def get_or_create_group_principal(group):
+    """Ensure existence of a group principal."""
+    principal = evalg.models.authorization.GroupPrincipal.query.filter_by(
+        group_id=group.id
+    ).first()
+
+    if principal:
+        current_app.logger.info('here!!')
+        current_app.logger.info(principal)
+        return principal
+    principal = evalg.models.authorization.GroupPrincipal()
+    principal.group_id = group.id
+    evalg.db.session.add(principal)
+    evalg.db.session.flush()
+    return principal
+
+
+def get_or_create_election_group_role(role_name, principal):
+    """Ensure existence of a election group role."""
+    role = evalg.models.authorization.ElectionGroupRole.query.filter_by(
+        name=role_name,
+        principal=principal,
+        global_role=True,
+    ).first()
+    if role:
+        current_app.logger.info('Found role for grant_id=%s', role.grant_id)
+        return role
+
+    current_app.logger.info('Creating role for grant_id=%s', role.grant_id)
+    role = evalg.models.authorization.ElectionGroupRole()
+    role.principal = principal
+    role.name = role_name
+    role.global_role = True
+
+    evalg.db.session.add(role)
+    evalg.db.session.flush()
+    current_app.logger.info(role)
 
 
 @click.command('init-authorization',
@@ -32,14 +75,17 @@ def create_groups():
     os.environ['EVALG_JOB_NAME'] = "init-authorization"
     config = flask.current_app.config
     mapping = config.get('FEIDE_ENTITLEMENT_MAPPING')
-    changes = False
-    for group_name in mapping:
-        current_app.logger.info('Checking if group %s exists', group_name)
-        if not get_group(group_name):
-            add_group(group_name)
-            changes = True
-    if changes:
-        evalg.db.session.commit()
+    current_app.logger.info('Initiating global authorization groups and '
+                            'principals')
+    for role_name in mapping:
+        current_app.logger.info('Creating role=%s', role_name)
+        group = get_or_create_group(role_name)
+        group_principal = get_or_create_group_principal(
+            group
+        )
+        role = get_or_create_election_group_role(role_name, group_principal)
+
+    evalg.db.session.commit()
 
 
 commands = tuple((
