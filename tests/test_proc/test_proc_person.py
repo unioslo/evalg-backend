68b565ee6c5fe0651bbc789a4bbd6636f8efc8f4
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2019-09-26 14:18:14 +0200

    Change test to create and find the same user, multiple tests matching foo
    Issue: EVALG-601

--- tests/test_proc/test_proc_person.py
+++ tests/test_proc/test_proc_person.py
@@ -2,6 +2,8 @@ from evalg.database.query import get_or_create
 from evalg.proc.person import search_persons
 
 
-def test_search_persons(db_session, persons):
-    results = search_persons(db_session, 'foo')
+def test_search_persons(db_session, person_generator):
+
+    person = person_generator()
+    results = search_persons(db_session, person.display_name)
     assert results.count() == 1
