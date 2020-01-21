4d23c1d92ccb9fb155304236c5444dfc523e33f1
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2020-01-21 09:52:06 +0100

    Flask command for sending status messages

--- evalg/cli/__init__.py
+++ evalg/cli/__init__.py
@@ -3,6 +3,7 @@ Command line interface for the evalg application.
 """
 from . import auth_cmds
 from . import db_cmds
+from . import email_cmds
 from . import flask_cmds
 from . import key_cmds
 from . import unit_cmds
@@ -12,6 +13,7 @@ def init_app(app):
     """ Add all evalg commands to flask application cli. """
     auth_cmds.init_app(app)
     db_cmds.init_app(app)
+    email_cmds.init_app(app)
     flask_cmds.init_app(app)
     key_cmds.init_app(app)
     unit_cmds.init_app(app)
