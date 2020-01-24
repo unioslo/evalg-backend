bbc3314b0f067f43cdbc684876598fbdc01d573d
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2020-01-24 10:16:57 +0100

    Added support for html email
    Issue: EVALG-709

--- evalg/mail/mailer.py
+++ evalg/mail/mailer.py
@@ -15,14 +15,34 @@ def load_template(template_name):
                              'templates')).get_template(template_name)
 
 
-def send_mail(template_name, subject, **kwargs):
-    """Send a jinja2 template email."""
+def send_mail(
+        template_name=None,
+        html_template_name=None,
+        to_addr=None,
+        subject='',
+        **kwargs):
+    """
+    Function for sending emails.
+
+    Minimum one of the templates needs to be defined. If both are defined,
+    the plain text will be added as the alternative.
+
+    :param template_name: Filename for the plain text mail jinja2 template
+    :param html_template_name: Filename for the html mail jinja2 template
+    :param to_addr: To address
+    :param subject: Mail subject
+    :param kwargs: Parameters used to generate mail from jinja2 templates
+    :return: None
+    """
     config = current_app.config
-    to_addr = kwargs.get('to_addr')
+
+    if not template_name or not html_template_name:
+        current_app.logger.error('Could not send email, no template given')
+        return
 
     if not to_addr:
-        current_app.logger.warning('Could not send email, not to_address '
-                                   'given')
+        current_app.logger.error('Could not send email, not to_address '
+                                 'given')
         return
 
     if not config.get('MAIL_ENABLE'):
@@ -33,7 +53,10 @@ def send_mail(template_name, subject, **kwargs):
     smtp_server = config.get('MAIL_SMTP_SERVER')
     smtp_port = config.get('MAIL_SMTP_PORT')
 
-    msg = email.mime.multipart.MIMEMultipart()
+    if template_name and html_template_name:
+        msg = email.mime.multipart.MIMEMultipart('alternative')
+    else:
+        msg = email.mime.multipart.MIMEMultipart()
     msg['From'] = config.get('MAIL_FROM_ADDR')
     msg['To'] = to_addr
     msg['Subject'] = subject
@@ -43,10 +66,16 @@ def send_mail(template_name, subject, **kwargs):
     if config.get('MAIL_REPLY_TO_ADDR'):
         msg['Reply-To'] = config.get('MAIL_REPLY_TO_ADDR')
 
-    text = load_template(template_name).render(**kwargs)
-    msg.attach(email.mime.text.MIMEText(text))
+    if template_name:
+        text = load_template(template_name).render(**kwargs)
+        msg.attach(email.mime.text.MIMEText(text, 'plain'))
+
+    if html_template_name:
+        html = load_template(html_template_name).render(**kwargs)
+        msg.attach(email.mime.text.MIMEText(html, 'html'))
 
     with smtplib.SMTP(host=smtp_server, port=smtp_port) as smtp:
         smtp.ehlo()
         smtp.starttls()
         smtp.send_message(msg)
+
