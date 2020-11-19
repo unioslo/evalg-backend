65dbacca39e3d07a87529b3aa76e356bf45c3096
Axel Sanner <axelsanner@gmail.com>
2020-11-19 21:59:26 +0100

    Changes for openshift
    2 new Dockerfiles
     - Dockerfile-k8s - docerfile for evalg on openshift
     - Dockerfile-k8s-worker - dockerfile for running celery on openshift
    
    update-harbor-image.sh to update to openshift labels if openshift is a
    parameter

--- update-harbor-image.sh
+++ update-harbor-image.sh
@@ -7,6 +7,46 @@ APP_NAME="valg-backend"
 CONTAINER="${REPO}/${PROJECT}/${APP_NAME}"
 IMAGE_TAG="${CONTAINER}:${GIT_BRANCH}-${GIT_SHA}"
 
+if [[ $1 == "openshift" ]]; then
+    APP_NAME="valg-backend-test"
+    CONTAINER="${REPO}/${PROJECT}/${APP_NAME}"
+    IMAGE_TAG="${CONTAINER}:${GIT_BRANCH}-${GIT_SHA}-${RANDOM}"
+    IMAGE_TAG_WORKER="${CONTAINER}:${GIT_BRANCH}-${GIT_SHA}-WORKER-${RANDOM}"
+
+    echo "Building $IMAGE_TAG"
+    docker build -f Dockerfile-k8s -t $IMAGE_TAG .
+
+    echo "Building $IMAGE_TAG"
+    docker build -f Dockerfile-k8s-worker -t $IMAGE_TAG_WORKER .
+
+    echo "Pushing $IMAGE_TAG"
+    docker push $IMAGE_TAG
+
+    echo "Pushing $IMAGE_TAG"
+    docker push $IMAGE_TAG_WORKER
+
+    echo "Tagging $IMAGE_TAG $CONTAINER:openshift"
+    docker tag $IMAGE_TAG $CONTAINER:openshift
+
+    echo "Tagging $IMAGE_TAG $CONTAINER:openshift"
+    docker tag $IMAGE_TAG_WORKER $CONTAINER:worker
+
+    echo "Tagging $IMAGE_TAG $CONTAINER:latest"
+    docker tag $IMAGE_TAG $CONTAINER:latest
+
+    echo "Pushing $CONTAINER:openshift"
+    docker push $CONTAINER:openshift
+
+    echo "Pushing $CONTAINER:openshift"
+    docker push $CONTAINER:worker
+
+    echo "Pushing $CONTAINER:latest"
+    docker push $CONTAINER:latest
+
+    exit 1
+fi
+
+
 echo "Building $IMAGE_TAG"
 docker build -f Dockerfile-staging -t $IMAGE_TAG .
 
