2ec9a21e96fd367addb61500cb8068704e3702c6
Sivert Hatteberg <sivertkh@ifi.uio.no>
2021-01-21 17:59:50 +0100

    Move the census file parser test to its own folder.
    Moved the correspondig conftest fixtures to cleanup the clutter
    in the main conftest file.
    
    Issue: EVALG-793

--- tests/test_graphql/conftest.py
+++ tests/test_graphql/conftest.py
@@ -1,6 +1,8 @@
+import io
 import pytest
 
 from graphene.test import Client
+from werkzeug.test import EnvironBuilder
 
 from evalg.graphql import schema
 
@@ -8,3 +10,28 @@ from evalg.graphql import schema
 @pytest.fixture(scope="session")
 def client():
     return Client(schema)
+
+
+def generate_census_file_builder(ids, file_ending, linebrake='\n'):
+    """Generate census test files."""
+    return EnvironBuilder(method='POST', data={
+        'file': (io.BytesIO(linebrake.join(ids).encode('utf-8')),
+                 'usernames.{}'.format(file_ending))})
+
+
+@pytest.fixture
+def uids():
+    """A list of uids."""
+    return ['pederaas', 'martekir', 'larsh', 'hansta']
+
+
+@pytest.fixture
+def feide_ids(uids):
+    """A list of feide ids."""
+    return ['{}@uio.no'.format(x) for x in uids]
+
+
+@pytest.fixture
+def feide_id_plane_text_census_builder(feide_ids):
+    """Plain text census file of feide ids."""
+    return generate_census_file_builder(feide_ids, 'txt')
