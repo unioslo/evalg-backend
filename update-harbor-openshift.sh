68dbe0e4ff60c6358f5aeda456b5df6a28db689a
Axel Sanner <axelsanner@gmail.com>
2021-02-22 14:33:26 +0100

    Change update-harbor-openshift.sh script
    To reflect changes in new harbor

--- update-harbor-openshift.sh
+++ update-harbor-openshift.sh
@@ -3,10 +3,15 @@ GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 GIT_SHA=`git rev-parse --short HEAD`
 REPO="harbor.uio.no"
 PROJECT="it-usit-int-drift"
-APP_NAME="valg-backend-test"
+
+APP_NAME="evalg-backend-k8s"
+WORKER_APP_NAME="evalg-worker-k8s"
+
 CONTAINER="${REPO}/${PROJECT}/${APP_NAME}"
+WORKER_CONTAINER="${REPO}/${PROJECT}/${WORKER_APP_NAME}"
+
 IMAGE_TAG="${CONTAINER}:${GIT_BRANCH}-${GIT_SHA}-${RANDOM}"
-IMAGE_TAG_WORKER="${CONTAINER}:${GIT_BRANCH}-${GIT_SHA}-WORKER-${RANDOM}"
+IMAGE_TAG_WORKER="${WORKER_CONTAINER}:${GIT_BRANCH}-${GIT_SHA}-WORKER-${RANDOM}"
 
 echo "Building $IMAGE_TAG"
 docker build -f Dockerfile-k8s -t $IMAGE_TAG .
@@ -21,19 +26,13 @@ echo "Pushing $IMAGE_TAG"
 docker push $IMAGE_TAG_WORKER
 
 echo "Tagging $IMAGE_TAG $CONTAINER:openshift"
-docker tag $IMAGE_TAG $CONTAINER:openshift
-
-echo "Tagging $IMAGE_TAG $CONTAINER:openshift"
-docker tag $IMAGE_TAG_WORKER $CONTAINER:worker
+docker tag $IMAGE_TAG_WORKER $WORKER_CONTAINER:latest
 
 echo "Tagging $IMAGE_TAG $CONTAINER:latest"
 docker tag $IMAGE_TAG $CONTAINER:latest
 
-echo "Pushing $CONTAINER:openshift"
-docker push $CONTAINER:openshift
-
-echo "Pushing $CONTAINER:openshift"
-docker push $CONTAINER:worker
+echo "Pushing $WORKER_CONTAINER:openshift"
+docker push $WORKER_CONTAINER:latest
 
 echo "Pushing $CONTAINER:latest"
 docker push $CONTAINER:latest
