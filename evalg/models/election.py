cc44521e0f32086f56cd8b4e637ca18dde0cf54d
Stein Elgethun <stein.elgethun@usit.uio.no>
2022-08-18 13:48:40 +0200

    Add quota rules for combined substitue and candidate
    Issue: EVALG-857

--- evalg/models/election.py
+++ evalg/models/election.py
@@ -458,7 +458,7 @@ class Election(AbstractElection):
         if self.election_group.meta["candidate_rules"].get("candidate_gender"):
             quota_names = self.meta["counting_rules"]["affirmative_action"]
             for quota_name in quota_names:
-                if quota_name == "gender_40":  # the only one supported so far
+                if quota_name == "gender_40":
                     males = []
                     females = []
                     min_value = 0
@@ -556,6 +556,57 @@ class Election(AbstractElection):
                                     len(females) - min_value_females,
                                 ]
                             ),
+                        )
+                    )
+                elif quota_name == "combined_substitute":
+                    males = []
+                    females = []
+                    min_value = 0
+                    min_value_substitutes = 0  # for uiostv .. etc
+                    for candidate in self.candidates:
+                        if candidate.meta["gender"] == "male":
+                            males.append(candidate)
+                        elif candidate.meta["gender"] == "female":
+                            females.append(candidate)
+                    if self.num_choosable in (0, 1):
+                        min_value = 0
+                    elif self.num_choosable in (2, 3):
+                        min_value = 1
+                    elif self.num_choosable in (4, 5):
+                        min_value = 2
+                    elif self.num_choosable in (6, 7, 8):
+                        min_value = 3
+                    elif self.num_choosable:
+                        min_value = math.ceil(0.4 * self.num_choosable)
+
+                    num_combined = self.num_choosable + self.num_substitutes
+                    if num_combined in (0, 1):
+                        min_value_substitutes = 0
+                    elif num_combined in (2, 3):
+                        min_value_substitutes = 1
+                    elif num_combined in (4, 5):
+                        min_value_substitutes = 2
+                    elif num_combined in (6, 7, 8):
+                        min_value_substitutes = 3
+                    elif num_combined:
+                        min_value_substitutes = math.ceil(0.4 * num_combined)
+                    # handle universal cases when members < min_value
+                    min_value_males = min([min_value, len(males)])
+                    min_value_females = min([min_value, len(females)])
+                    quotas.append(
+                        QuotaGroup(
+                            {"en": "Males", "nn": "Menn", "nb": "Menn"},
+                            males,
+                            min_value_males,
+                            min([min_value_substitutes, len(males)]),
+                        )
+                    )
+                    quotas.append(
+                        QuotaGroup(
+                            {"en": "Females", "nn": "Kvinner", "nb": "Kvinner"},
+                            females,
+                            min_value_females,
+                            min([min_value_substitutes, len(females),]),
                         )
                     )
         return quotas
